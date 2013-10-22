b1fac1cb3edfecba3c0b017e77486dc8f279041f
Maksim Pugach <pugach.m@gmail.com>
2013-10-22 18:22:15 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VedAkademBase2ndEd::Application.config.secret_key_base = 'a5e87ea3fdc1ddd6dc8fe73e22e93d53f4ea86152f9d9450efc448afbea3899cf8a6ef5abaa7f2efe4728e693cd0fdedfe5e5c4d181489624d8c4aa324ba7b89'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VedAkademBase2ndEd::Application.config.session_store :cookie_store, key: '_ved_akadem_base_2nd_ed_session'
