b979ebc2b259be062a4d3ddf34d1373f287908dc
Maksim Pugach <pugach.m@gmail.com>
2013-11-02 12:01:58 +0200

    hide secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-VedAkademBase2ndEd::Application.config.secret_key_base = 'a5e87ea3fdc1ddd6dc8fe73e22e93d53f4ea86152f9d9450efc448afbea3899cf8a6ef5abaa7f2efe4728e693cd0fdedfe5e5c4d181489624d8c4aa324ba7b89'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
