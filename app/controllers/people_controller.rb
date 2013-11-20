dc6f508b50dbbf91a47b1ddb2acb63672b5131d2
Maksim Pugach <pugach.m@gmail.com>
2013-11-20 13:10:13 +0200

    people#create and begining of Add page

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,4 +2,10 @@ class PeopleController < ApplicationController
   def add
     @person = Person.new
   end
+
+  def create
+    #@person = Person.new(params.require(:person).permit(:name))
+    #@person.save
+    redirect_to(action: :add)
+  end
 end
--- spec/views/people/add.html.erb_spec.rb
+++ spec/views/people/add.html.erb_spec.rb
@@ -1,8 +1,15 @@
 require 'spec_helper'
 
 describe "people/add.html.erb" do
-  it "should have the right title" do
-    visit '/people/add'
-    expect(page).to have_title("Kiev Vedic Akademy DataBase | Add New Person")
-  end
+
+  before { visit people_add_path }
+
+  subject { page }
+
+  it { should have_title("Kiev Vedic Akademy DataBase | Add New Person") }
+  it { should have_selector('form.new_person') }
+  it { should have_selector('label', text: "Name") }
+  it { should have_selector('input#person_name') }
+  it { should have_selector('input.btn') }
+
 end
