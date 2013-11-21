f4921880a8634f92a04590e4b7a9a8f874369003
Maksim Pugach <pugach.m@gmail.com>
2013-11-21 10:21:09 +0200

    Basic person#add view.

--- spec/views/people/add.html.erb_spec.rb
+++ spec/views/people/add.html.erb_spec.rb
@@ -11,5 +11,27 @@ describe "people/add.html.erb" do
   it { should have_selector('label', text: "Name") }
   it { should have_selector('input#person_name') }
   it { should have_selector('input.btn') }
+  it { should have_selector('label', text: "Middle name") }
+  it { should have_selector('input#person_middle_name') }
+  it { should have_selector('label', text: "Surname") }
+  it { should have_selector('input#person_surname') }
+  it { should have_selector('label', text: "Spiritual name") }
+  it { should have_selector('input#person_spiritual_name') }
+  it { should have_selector('label', text: "Telephone") }
+  it { should have_selector('input#person_telephone') }
+  it { should have_selector('label', text: "Email") }
+  it { should have_selector('input#person_email') }
+  it { should have_selector('label', text: "Gender") }
+  it { should have_selector('select#person_gender') }
+  it { should have_selector('label', text: "Birthday") }
+  it { should have_selector('select#person_birthday_1i') }
+  it { should have_selector('select#person_birthday_2i') }
+  it { should have_selector('select#person_birthday_3i') }
+  it { should have_selector('label', text: "Education and job") }
+  it { should have_selector('input#person_edu_and_work') }
+  it { should have_selector('label', text: "Emergency contact") }
+  it { should have_selector('input#person_emergency_contact') }
+  xit { should have_selector('label', text: "Photo") }
+  xit { should have_selector('input#person_photo') }
 
 end
