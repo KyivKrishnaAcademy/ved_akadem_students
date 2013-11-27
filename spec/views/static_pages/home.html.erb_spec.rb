f59c191e92413248a0b9d0b1f1e866bd67ed59a6
Maksim Pugach <pugach.m@gmail.com>
2013-11-27 15:19:52 +0200

    Typo fix

--- spec/views/static_pages/home.html.erb_spec.rb
+++ spec/views/static_pages/home.html.erb_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe "static_pages/home.html.erb" do
   it "should have the right title" do
     visit '/static_pages/home'
-    expect(page).to have_title("Kiev Vedic Akademy DataBase | Home")
+    expect(page).to have_title("Kyiv Vedic Akademy DataBase | Home")
   end
 end
