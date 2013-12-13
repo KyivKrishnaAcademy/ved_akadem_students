bc63dc3a051e8191f8c8521f652305648e74f3c6
Maksim Pugach <pugach.m@gmail.com>
2013-12-13 12:44:58 +0200

    Better spec doc

--- spec/models/akadem_group_spec.rb
+++ spec/models/akadem_group_spec.rb
@@ -1,34 +1,42 @@
 require 'spec_helper'
 
 describe AkademGroup do
-  it { should have_db_column(:group_name        ).of_type(:string ) }
-  it { should have_db_column(:group_description ).of_type(:string ) }
-  it { should have_db_column(:establ_date       ).of_type(:date   ) }
-
-  it { should have_many(:group_participations ).dependent(:destroy              ) }
-  it { should have_many(:student_profiles     ).through(  :group_participations ) }
-  it { should have_many(:class_schedules      ).dependent(:destroy              ) }
-
-  it { should validate_uniqueness_of(:group_name) }
-  it { should validate_presence_of(  :group_name) }
-
-  it "group_name upcased" do
-    create_akadem_group(group_name: "шб13-1")
-      .group_name.should == "ШБ13-1"
+  describe "DB table" do
+    it { should have_db_column(:group_name        ).of_type(:string ) }
+    it { should have_db_column(:group_description ).of_type(:string ) }
+    it { should have_db_column(:establ_date       ).of_type(:date   ) }
   end
 
-  VALID_NAMES   = %w[ ШБ13-1 БШ12-4 ЗШБ11-1 ]
-  INVALID_NAMES = %w[ 12-2 ШБ-1 БШ112 ШБ11- ]
+  describe "associations" do
+    it { should have_many(:group_participations ).dependent(:destroy              ) }
+    it { should have_many(:student_profiles     ).through(  :group_participations ) }
+    it { should have_many(:class_schedules      ).dependent(:destroy              ) }
+  end
 
-  it do
-    VALID_NAMES.each do |name|
-      should      allow_value(name).for(:group_name)
+  describe "validation" do
+    it { should validate_uniqueness_of(:group_name) }
+    it { should validate_presence_of(  :group_name) }
+
+    VALID_NAMES   = %w[ ШБ13-1 БШ12-4 ЗШБ11-1 ]
+    INVALID_NAMES = %w[ 12-2 ШБ-1 БШ112 ШБ11- ]
+
+    it "allows valid addresses" do
+      VALID_NAMES.each do |name|
+        should      allow_value(name).for(:group_name)
+      end
+    end
+
+    it "disallows invalid names" do
+      INVALID_NAMES.each do |name|
+        should_not  allow_value(name).for(:group_name)
+      end
     end
   end
 
-  it do
-    INVALID_NAMES.each do |name|
-      should_not  allow_value(name).for(:group_name)
+  describe "before save processing" do
+    it "upcases :group_name" do
+      create_akadem_group(group_name: "шб13-1")
+        .group_name.should == "ШБ13-1"
     end
   end
 end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,80 +1,90 @@
 require 'spec_helper'
 
 describe Person do
-
   after(:all) { Person.destroy_all }
 
-  it { should have_db_column(:name              ).of_type(:string   ) }
-  it { should have_db_column(:middle_name       ).of_type(:string   ) }
-  it { should have_db_column(:surname           ).of_type(:string   ) }
-  it { should have_db_column(:spiritual_name    ).of_type(:string   ) }
-  it { should have_db_column(:telephone         ).of_type(:integer  ) }
-  it { should have_db_column(:email             ).of_type(:string   ) }
-  it { should have_db_column(:gender            ).of_type(:boolean  ) }
-  it { should have_db_column(:birthday          ).of_type(:date     ) }
-  it { should have_db_column(:emergency_contact ).of_type(:string   ) }
-  it { should have_db_column(:photo             ).of_type(:text     ) }
-  it { should have_db_column(:profile_fullness  ).of_type(:boolean  ) }
-  it { should have_db_column(:edu_and_work      ).of_type(:text     ) }
-
-  it { should have_one(:student_profile).dependent(:destroy) }
-
-  it { should validate_presence_of(:name      ) }
-  it { should validate_presence_of(:surname   ) }
-  it { should validate_presence_of(:telephone ) }
-
-  it { should     allow_value(  true, false   ).for(:gender) }
-  it { should_not allow_value(  nil           ).for(:gender) }
-
-  it { should ensure_length_of(:name            ).is_at_most(50) }
-  it { should ensure_length_of(:surname         ).is_at_most(50) }
-  it { should ensure_length_of(:middle_name     ).is_at_most(50) }
-  it { should ensure_length_of(:spiritual_name  ).is_at_most(50) }
-
-  it { should validate_numericality_of(:telephone ) }
-  it { should validate_uniqueness_of(  :telephone ) }
-  it { should ensure_inclusion_of(     :telephone )
-        .in_range(100_000_000_000..999_999_999_999)
-        .with_low_message(  /must be greater than/)
-        .with_high_message( /must be less than/   ) }
-
-  INVALID_ADDRESSES = %w[
-    user@foo,com     user_at_foo.org
-    example.user@foo.foo@bar_baz.com
-    foo@bar+baz.com
-  ]
-
-  VALID_ADDRESSES   = %w[
-    user@foo.COM A_US-ER@f.b.org
-    frst.lst@foo.jp   a+b@baz.cn
-  ]
-
-  it do
-    INVALID_ADDRESSES.each  do |invalid_address|
-      should_not  allow_value(invalid_address).for(:email)
-    end
+  describe "DB table" do
+    it { should have_db_column(:name              ).of_type(:string   ) }
+    it { should have_db_column(:middle_name       ).of_type(:string   ) }
+    it { should have_db_column(:surname           ).of_type(:string   ) }
+    it { should have_db_column(:spiritual_name    ).of_type(:string   ) }
+    it { should have_db_column(:telephone         ).of_type(:integer  ) }
+    it { should have_db_column(:email             ).of_type(:string   ) }
+    it { should have_db_column(:gender            ).of_type(:boolean  ) }
+    it { should have_db_column(:birthday          ).of_type(:date     ) }
+    it { should have_db_column(:emergency_contact ).of_type(:string   ) }
+    it { should have_db_column(:photo             ).of_type(:text     ) }
+    it { should have_db_column(:profile_fullness  ).of_type(:boolean  ) }
+    it { should have_db_column(:edu_and_work      ).of_type(:text     ) }
   end
 
-  it do
-    VALID_ADDRESSES.each    do |valid_address|
-      should      allow_value(  valid_address).for(:email)
-    end
+  describe "association" do
+    it { should have_one(:student_profile).dependent(:destroy) }
   end
 
-  describe "email" do
-    it "should be downcased" do
-      VALID_ADDRESSES.each do |valid_address|
-        create_person(email: valid_address).email.should == valid_address.downcase
+  describe "validation" do
+    context :gender do
+      it { should     allow_value(  true, false   ).for(:gender) }
+      it { should_not allow_value(  nil           ).for(:gender) }
+    end
+
+    context :telephone do
+      it { should validate_presence_of(:telephone )     }
+      it { should validate_numericality_of(:telephone ) }
+      it { should validate_uniqueness_of(  :telephone ) }
+      it { should ensure_inclusion_of(     :telephone )
+            .in_range(100_000_000_000..999_999_999_999)
+            .with_low_message(  /must be greater than/)
+            .with_high_message( /must be less than/   ) }
+    end
+
+    context :email do
+      INVALID_ADDRESSES = %w[
+        user@foo,com     user_at_foo.org
+        example.user@foo.foo@bar_baz.com
+        foo@bar+baz.com
+      ]
+
+      VALID_ADDRESSES   = %w[
+        user@foo.COM A_US-ER@f.b.org
+        frst.lst@foo.jp   a+b@baz.cn
+      ]
+
+      it "allows valid addresses" do
+        INVALID_ADDRESSES.each  do |invalid_address|
+          should_not  allow_value(invalid_address).for(:email)
+        end
+      end
+
+      it "disallows invalid addresses" do
+        VALID_ADDRESSES.each    do |valid_address|
+          should      allow_value(  valid_address).for(:email)
+        end
+      end
+
+      it "allows empty value" do
+        should allow_value('').for(:email)
       end
     end
 
-    it "allows empty value" do
-      should allow_value('').for(:email)
+    context "name, surname, middle_name, spiritual_name" do
+      it { should validate_presence_of(:name      ) }
+      it { should validate_presence_of(:surname   ) }
+
+      it { should ensure_length_of(:name            ).is_at_most(50) }
+      it { should ensure_length_of(:surname         ).is_at_most(50) }
+      it { should ensure_length_of(:middle_name     ).is_at_most(50) }
+      it { should ensure_length_of(:spiritual_name  ).is_at_most(50) }
     end
   end
 
-  describe "columns" do
-    it "should be downcased and titelized" do
+  describe "before save processing" do
+    it "downcases :email" do
+      create_person(email: "A_US-ER@f.B.org")
+        .email.should == "a_us-er@f.b.org"
+    end
+
+    it "downcases and titelizes :name, :surname, :middle_name, :spiritual_name" do
       name, surname, mname, spname = 'имЯ', 'фАмИлиЯ', 'ОтчествО', 'АдиДасаДаса ДаС'
       p = FactoryGirl.create(:person,
           name:           name    ,
@@ -88,5 +98,4 @@ describe Person do
       p.spiritual_name.should ==  'Адидасадаса Дас'
     end
   end
-
 end
