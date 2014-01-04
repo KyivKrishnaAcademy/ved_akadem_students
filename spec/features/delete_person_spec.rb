93ad396de5607bf8bc9390f2840e649f323de3e5
Maksim Pugach <pugach.m@gmail.com>
2014-01-04 16:13:13 +0200

    Reorganize #delete and #edit Person integration tests

--- spec/features/delete_person_spec.rb
+++ spec/features/delete_person_spec.rb
@@ -1,12 +1,14 @@
 require 'spec_helper'
 
 feature "Delete person:", js: true do
-  before(:all) { create_person      }
-  after(:all)  { Person.destroy_all }
+  after (:all)  { Person.destroy_all }
+  before(:all)  { 2.times { create_person } }
+  before(:each) { visit person_path(Person.last) }
 
   scenario do
-    visit person_path(Person.last)
-    expect{ click_link "Delete" }.to change{Person.count}.by(-1)
+    click_link "Delete"
     page.should have_selector('section.alert-success', text: 'Person record deleted!')
   end
+
+  scenario { expect{ click_link "Delete" }.to change{Person.count}.by(-1) }
 end
--- spec/features/edit_person_spec.rb
+++ spec/features/edit_person_spec.rb
@@ -1,18 +1,19 @@
 require 'spec_helper'
 
 feature "Edit person:" do
-  before(:all) { create_person      }
   after(:all)  { Person.destroy_all }
+
+  let(:p) { create_person }
 
   subject { page }
 
-  scenario do
-    visit person_path(Person.last)
+  before(:each) do
+    visit person_path(p)
     click_link "Edit"
     fill_in 'Spiritual name', with: 'AdiDasa dasa dasa anudasa'
     click_button "Update Person"
-
-    should have_content('Spiritual name: Adidasa Dasa Dasa Anudasa')
-    should have_selector('section.alert-success', text: 'Person was successfully updated.')
   end
+
+  scenario { should have_content('Spiritual name: Adidasa Dasa Dasa Anudasa') }
+  scenario { should have_selector('section.alert-success', text: 'Person was successfully updated.') }
 end
