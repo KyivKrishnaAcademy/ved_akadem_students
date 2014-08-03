8c70f2f04702f8a1b0c41b1116ada01c0a905b47
Maksym Pugach <pugach.m@gmail.com>
2014-08-03 13:37:44 +0300

    [locales] use FactoryGirl Syntax Methods

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -51,6 +51,7 @@ Spork.prefork do
   ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
   RSpec.configure do |config|
+    config.include FactoryGirl::Syntax::Methods
     # ## Mock Framework
     #
     # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
--- spec/support/get_models.rb
+++
@@ -1,7 +0,0 @@
-def build(model, h={})
-  FactoryGirl.build(model, h)
-end
-
-def create(model, h={})
-  FactoryGirl.create(model, h)
-end
