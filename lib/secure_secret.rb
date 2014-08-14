79fd01b61c3d27a1422b3e3e6a86e72c9bc1b17a
Maksym Pugach <pugach.m@gmail.com>
2014-08-14 21:58:26 +0300

    [registration] refactor location of SecureSecret module

--- lib/secure_secret.rb
+++ lib/secure_secret.rb
@@ -3,13 +3,14 @@ module SecureSecret
 
   def self.secure_token(filename)
     token_file = Rails.root.join(filename)
+
     if File.exist?(token_file)
-      # Use the existing token.
       File.read(token_file).chomp
     else
-      # Generate a new token and store it in token_file.
       token = SecureRandom.hex(64)
+
       File.write(token_file, token)
+
       token
     end
   end
