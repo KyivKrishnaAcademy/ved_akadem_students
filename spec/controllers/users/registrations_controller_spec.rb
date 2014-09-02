40ff28e55e3dada4d3ce49e25b5809025e0f1ef2
Maksym Pugach <pugach.m@gmail.com>
2014-09-02 22:39:20 +0300

    [admin_registration] add test of redirection to crops controller on registration with image

--- spec/controllers/users/registrations_controller_spec.rb
+++ spec/controllers/users/registrations_controller_spec.rb
@@ -1,9 +1,7 @@
 require 'spec_helper'
 
 describe Users::RegistrationsController do
-  Given { request.env["devise.mapping"] = Devise.mappings[:person] }
-
-  When { sign_in :person, @person }
+  Given { request.env['devise.mapping'] = Devise.mappings[:person] }
 
   shared_examples_for :hide_person do
     When { delete :destroy }
@@ -16,6 +14,8 @@ describe Users::RegistrationsController do
   end
 
   describe 'destroy' do
+    When { sign_in :person, @person }
+
     context 'person is not a student or teacher' do
       Given { @person = create :person }
 
@@ -32,6 +32,25 @@ describe Users::RegistrationsController do
       Given { @person = create :person, :teacher, email: 'some@example.com' }
 
       it_behaves_like :hide_person
+    end
+  end
+
+  describe 'direct to crop path' do
+    context 'has photo' do
+      When { post :create, person: build(:person).attributes.merge(password: 'password',
+                                                                   password_confirmation: 'password',
+                                                                   photo: 'test.png',
+                                                                   telephones_attributes: { '0' => { phone: '1234567890'}}) }
+
+      Then { response.should redirect_to(crop_image_path(assigns(:person).id)) }
+    end
+
+    context 'has no photo' do
+      When { post :create, person: build(:person).attributes.merge(password: 'password',
+                                                                   password_confirmation: 'password',
+                                                                   telephones_attributes: { '0' => { phone: '1234567890'}}) }
+
+      Then { response.should redirect_to(root_path) }
     end
   end
 end
