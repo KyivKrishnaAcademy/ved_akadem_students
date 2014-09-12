31cd3c14dc0370fd922c0d44978e2e05ecf0cb17
Maksym Pugach <pugach.m@gmail.com>
2014-09-12 12:49:19 +0300

    [various] style sign in button and redirect from home without flash if user is not signed int

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,9 @@
 class StaticPagesController < ApplicationController
   include StudyApplicationable
 
-  before_action :authenticate_person!, only: :home
   before_action :set_programs_and_new_application, only: :home
+
+  def home
+    redirect_to new_person_session_path if current_person.blank?
+  end
 end
