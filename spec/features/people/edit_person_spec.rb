17fe3b90f349dc197742fd42d3af1bd68a2f563c
Maksym Pugach <pugach.m@gmail.com>
2014-09-18 15:21:48 +0300

    [styling] test to pass with different seed

--- spec/features/people/edit_person_spec.rb
+++ spec/features/people/edit_person_spec.rb
@@ -3,23 +3,21 @@ require 'rails_helper'
 describe 'Edit person:' do
   subject { page }
 
-  before do
-    login_as_admin
-    visit person_path(create(:person, birthday: '2008-10-08'))
-    click_link 'Edit'
-  end
+  When { login_as_admin }
+  When { visit person_path(create(:person, birthday: '2008-10-08')) }
+  When { click_link 'Edit' }
 
   context 'When values are valid:' do
     [
-      { field: I18n.t('simple_form.labels.defaults.spiritual_name')   , value: 'AdiDasa dasa'   , test_field: 'Spiritual name: Adidasa Dasa' },
-      { field: I18n.t('simple_form.labels.defaults.name')             , value: 'алексей'        , test_field: 'Name: Алексей'                },
-      { field: I18n.t('simple_form.labels.defaults.middle_name')      , value: 'иванович'       , test_field: 'Middle name: Иванович'        },
-      { field: I18n.t('simple_form.labels.defaults.surname')          , value: 'евгеньев'       , test_field: 'Surname: Евгеньев'            },
-      { field: I18n.t('simple_form.labels.defaults.email')            , value: 'alex@PAMHO.net' , test_field: 'Email: alex@pamho.net'        },
-      { field: I18n.t('simple_form.labels.defaults.phone')            , value: '380692223344'   , test_field: 'Telephone 1: 380692223344'    },
-      { field: I18n.t('simple_form.labels.defaults.education')        , value: 'some'           , test_field: 'Education: some'              },
-      { field: I18n.t('simple_form.labels.defaults.work')             , value: 'other'          , test_field: 'Work: other'                  },
-      { field: I18n.t('simple_form.labels.defaults.emergency_contact'), value: 'дядя Петя'      , test_field: 'Emergency contact: дядя Петя' }
+      { field: 'person_telephones_attributes_0_phone', value: '380692223344', test_field: 'Telephone 1: 380692223344'},
+      { field: 'person[spiritual_name]'   , value: 'AdiDasa dasa'   , test_field: 'Spiritual name: Adidasa Dasa' },
+      { field: 'person[name]'             , value: 'алексей'        , test_field: 'Name: Алексей'                },
+      { field: 'person[middle_name]'      , value: 'иванович'       , test_field: 'Middle name: Иванович'        },
+      { field: 'person[surname]'          , value: 'евгеньев'       , test_field: 'Surname: Евгеньев'            },
+      { field: 'person[email]'            , value: 'alex@PAMHO.net' , test_field: 'Email: alex@pamho.net'        },
+      { field: 'person[education]'        , value: 'some'           , test_field: 'Education: some'              },
+      { field: 'person[work]'             , value: 'other'          , test_field: 'Work: other'                  },
+      { field: 'person[emergency_contact]', value: 'дядя Петя'      , test_field: 'Emergency contact: дядя Петя' }
     ].each do |h|
       it_behaves_like :valid_fill_in, h, 'Person'
     end
@@ -36,17 +34,17 @@ describe 'Edit person:' do
     end
 
     describe 'Gender' do
-      it_behaves_like :valid_select, 'Person', I18n.t('simple_form.labels.defaults.gender'), 'Male'  , 'Gender: Male'
-      it_behaves_like :valid_select, 'Person', I18n.t('simple_form.labels.defaults.gender'), 'Female', 'Gender: Female'
+      it_behaves_like :valid_select, 'Person', 'person[gender]', 'Male'  , 'Gender: Male'
+      it_behaves_like :valid_select, 'Person', 'person[gender]', 'Female', 'Gender: Female'
     end
   end
 
   context 'When values are invalid:' do
     [
-      { field: I18n.t('simple_form.labels.defaults.phone')  , value: '501112233' },
-      { field: I18n.t('simple_form.labels.defaults.email')  , value: '@@.com@' },
-      { field: I18n.t('simple_form.labels.defaults.name')   , value: '' },
-      { field: I18n.t('simple_form.labels.defaults.surname'), value: '' }
+      { field: 'person_telephones_attributes_0_phone', value: '501112233' },
+      { field: 'person[email]'  , value: '@@.com@' },
+      { field: 'person[name]'   , value: '' },
+      { field: 'person[surname]', value: '' }
     ].each do |h|
       describe h[:field] do
         When do
