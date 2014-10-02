bef1910a8335ed2f964e36732ad6ed96c112f54a
Maksym Pugach <pugach.m@gmail.com>
2014-10-02 18:20:08 +0300

    [active_record_localization] localize shared errors

---
+++ config/locales/plurals.rb
@@ -0,0 +1,5 @@
+#https://github.com/svenfuchs/i18n/blob/master/test/test_data/locales/plurals.rb
+
+common = { i18n: { plural: { keys: [:one, :few, :many, :other], rule: lambda { |n| n % 10 == 1 && n % 100 != 11 ? :one : [2, 3, 4].include?(n % 10) && ![12, 13, 14].include?(n % 100) ? :few : n % 10 == 0 || [5, 6, 7, 8, 9].include?(n % 10) || [11, 12, 13, 14].include?(n % 100) ? :many : :other } } } }
+
+{ ru: common, uk: common }
