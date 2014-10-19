a7db474f341fa5287f0da092efcadadf34a135a7
Maksym Pugach <pugach.m@gmail.com>
2014-10-19 21:46:24 +0300

    [add_fields_to_person] add localized fields to register new person form

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,5 @@
 class Person < ActiveRecord::Base
-  MARITAL_STATUSES = %w[single in_relationship married divorced widowed]
+  MARITAL_STATUSES = %i[single in_relationship married divorced widowed]
 
   attr_accessor :skip_password_validation, :photo_upload_height, :photo_upload_width
   attr_accessor :crop_x, :crop_y, :crop_w, :crop_h
