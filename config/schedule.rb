554ff1302cc51dd3685147da21fb44fb1957f82f
Maksym Pugach <pugach.m@gmail.com>
2014-12-04 16:19:01 +0200

    perform only one backup job

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,10 +4,10 @@ every '5 0 1 * *' do
   exec 'backup perform -t monthly'
 end
 
-every '5 0 * * 1' do
+every '5 0 2-31 * 1' do
   exec 'backup perform -t weekly'
 end
 
-every '5 0 * * *' do
+every '5 0 2-31 * 2-7' do
   exec 'backup perform -t daily'
 end
