6274c37eed116b192d341ebb86d5c8c6d0687399
Maksym Pugach <pugach.m@gmail.com>
2014-12-14 21:42:59 +0200

    [#39] StudyApplicationsController add admin user destroy tests

--- spec/controllers/study_applications_controller_spec.rb
+++ spec/controllers/study_applications_controller_spec.rb
@@ -13,7 +13,7 @@ describe StudyApplicationsController do
       Given(:study_application) { double(StudyApplication, person_id: 1, program_id: 1 ) }
       Given(:action) { delete :destroy, id: 1, format: :js }
 
-      Given { allow_any_instance_of(Pundit::PolicyFinder).to receive(:policy).and_return(StudyApplicationPolicy) }
+      Given { allow(study_application).to receive(:class).and_return(StudyApplication) }
       Given { allow(StudyApplication).to receive(:find).with('1').and_return(study_application) }
       Given { expect(study_application).not_to receive(:destroy) }
       Given { expect_any_instance_of(Person).not_to receive(:remove_application_questionnaires) }
@@ -30,77 +30,108 @@ describe StudyApplicationsController do
     end
   end
 
-  describe 'regular user' do
-    shared_examples :not_athorized do
-      Then  { expect(response.status).to eq(302) }
-      And   { expect(response).not_to render_template('_common') }
-      And   { is_expected.to set_the_flash[:danger].to(I18n.t(:not_authorized)) }
-    end
-
+  describe 'with user' do
     shared_examples :athorized do
       Then  { expect(response.status).to eq(200) }
       And   { expect(response).to render_template('_common') }
     end
 
-    Given(:person) { double(Person, id: 1, roles: []) }
+    shared_examples :athorized_destroy do
+      Given { allow(study_application).to receive(:class).and_return(StudyApplication) }
+      Given { expect(study_application).to receive(:destroy) }
+      Given { allow(study_application).to receive(:person).and_return(person) }
+      Given { expect(person).to receive(:remove_application_questionnaires) }
 
-    Given { allow(person).to receive(:can_act?).with('study_application:create') { false } }
+      it_behaves_like :athorized
+    end
+
+    Given(:person) { double(Person, id: 1, roles: roles) }
 
     Given { allow(request.env['warden']).to receive(:authenticate!) { person } }
     Given { allow(controller).to receive(:current_person) { person } }
 
-    context 'destroy' do
-      Given { allow_any_instance_of(Pundit::PolicyFinder).to receive(:policy).and_return(StudyApplicationPolicy) }
-      Given { allow(StudyApplication).to receive(:find).with('1').and_return(study_application) }
-
-      When  { delete :destroy, id: 1, format: :js }
-
-      context 'allow own' do
-        Given(:study_application) { double(StudyApplication, person_id: person.id, program_id: 1 ) }
-
-        Given { expect(study_application).to receive(:destroy) }
-        Given { allow(study_application).to receive(:person).and_return(person) }
-        Given { expect(person).to receive(:remove_application_questionnaires) }
-
-        it_behaves_like :athorized
+    describe 'regular user' do
+      shared_examples :not_athorized do
+        Then  { expect(response.status).to eq(302) }
+        And   { expect(response).not_to render_template('_common') }
+        And   { is_expected.to set_the_flash[:danger].to(I18n.t(:not_authorized)) }
       end
 
-      context 'disallow others' do
-        Given(:study_application) { double(StudyApplication, person_id: person.id + 1, program_id: 1 ) }
+      Given(:roles) { [] }
 
-        Given { expect(study_application).not_to receive(:destroy) }
-        Given { expect(person).not_to receive(:remove_application_questionnaires) }
+      Given { allow(person).to receive(:can_act?).with('study_application:create') { false } }
 
-        it_behaves_like :not_athorized
+      context 'destroy' do
+        Given { allow(StudyApplication).to receive(:find).with('1').and_return(study_application) }
+
+        When  { delete :destroy, id: 1, format: :js }
+
+        context 'allow own' do
+          Given(:study_application) { double(StudyApplication, person_id: person.id, program_id: 1 ) }
+
+          it_behaves_like :athorized_destroy
+        end
+
+        context 'disallow others' do
+          Given(:study_application) { double(StudyApplication, person_id: person.id + 1, program_id: 1 ) }
+
+          Given { allow(study_application).to receive(:class).and_return(StudyApplication) }
+          Given { expect(study_application).not_to receive(:destroy) }
+          Given { expect(person).not_to receive(:remove_application_questionnaires) }
+
+          it_behaves_like :not_athorized
+        end
+      end
+
+      context 'create' do
+        context 'allow own' do
+          Given { expect_any_instance_of(StudyApplication).to receive(:save).and_return(true) }
+          Given { expect_any_instance_of(StudyApplication).to receive(:person).and_return(person) }
+          Given { expect(person).to receive(:add_application_questionnaires) }
+
+          When  { post :create, study_application: { person_id: person.id, program_id: 1 }, format: :js }
+
+          it_behaves_like :athorized
+        end
+
+        context 'disallow others' do
+          Given { expect_any_instance_of(StudyApplication).not_to receive(:save) }
+          Given { expect(person).not_to receive(:add_application_questionnaires) }
+
+          When  { post :create, study_application: { person_id: person.id + 1, program_id: 1 }, format: :js }
+
+          it_behaves_like :not_athorized
+        end
       end
     end
 
-    context 'create' do
-      context 'allow own' do
-        Given { expect_any_instance_of(StudyApplication).to receive(:save).and_return(true) }
-        Given { expect_any_instance_of(StudyApplication).to receive(:person).and_return(person) }
-        Given { expect(person).to receive(:add_application_questionnaires) }
+    describe 'admin user' do
+      Given(:roles) { double('roles', any?: true, name: 'Role') }
 
-        When  { post :create, study_application: { person_id: person.id, program_id: 1 }, format: :js }
+      Given { allow(roles).to receive_message_chain(:select, :distinct, :map, :flatten) { ['study_application:destroy'] } }
+      Given { allow(person).to receive(:can_act?).with('study_application:create') { true } }
 
-        it_behaves_like :athorized
+      context 'destroy' do
+        Given { allow(StudyApplication).to receive(:find).with('1').and_return(study_application) }
+
+        When  { delete :destroy, id: 1, format: :js }
+
+        context 'allow own' do
+          Given(:study_application) { double(StudyApplication, person_id: person.id, program_id: 1 ) }
+
+          it_behaves_like :athorized_destroy
+        end
+
+        context 'allow others' do
+          Given(:study_application) { double(StudyApplication, person_id: person.id + 1, program_id: 1) }
+
+          it_behaves_like :athorized_destroy
+        end
       end
 
-      context 'disallow others' do
-        Given { expect_any_instance_of(StudyApplication).not_to receive(:save) }
-        Given { expect(person).not_to receive(:add_application_questionnaires) }
-
-        When  { post :create, study_application: { person_id: person.id + 1, program_id: 1 }, format: :js }
-
-        it_behaves_like :not_athorized
+      context 'create' do
+        #TODO
       end
     end
-  end
-
-  describe 'admin user' do
-    #Given(:admin) { create :person, :admin }
-    #
-    #When { sign_in :person, admin }
-
   end
 end
