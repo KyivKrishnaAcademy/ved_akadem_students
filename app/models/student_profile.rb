0881ef58db987c445d182054c6587b0409ccdbde
Maksym Pugach <pugach.m@gmail.com>
2015-01-09 13:21:38 +0200

    [#20] StudentProfile#move_to_group

--- app/models/student_profile.rb
+++ app/models/student_profile.rb
@@ -4,4 +4,12 @@ class StudentProfile < ActiveRecord::Base
   has_many    :akadem_groups       , through:   :group_participations
   has_many    :attendances         , dependent: :destroy
   has_many    :class_schedules     , through:   :attendances
+
+  def move_to_group(akadem_group)
+    prev = group_participations.where(leave_date: nil).first
+
+    prev.leave! if prev.present?
+
+    akadem_groups << akadem_group
+  end
 end
--- spec/models/student_profile_spec.rb
+++ spec/models/student_profile_spec.rb
@@ -8,4 +8,25 @@ describe StudentProfile do
     Then { is_expected.to have_many(:attendances          ).dependent(:destroy              ) }
     Then { is_expected.to have_many(:class_schedules      ).through(  :attendances          ) }
   end
+
+  describe '#move_to_group' do
+    Given { @sp     = create :student_profile }
+    Given { @new_ag = create :akadem_group }
+
+    When  { @sp.move_to_group(@new_ag) }
+
+    context 'first group' do
+      Then { expect(@sp.akadem_groups).to eq([@new_ag]) }
+      And  { expect(@sp.group_participations.find_by(akadem_group_id: @new_ag.id).leave_date).to be_nil }
+    end
+
+    context 'second group' do
+      Given { @old_ag = create :akadem_group }
+      Given { @sp.akadem_groups << @old_ag }
+
+      Then  { expect(@sp.akadem_groups).to eq([@new_ag, @old_ag]) }
+      And   { expect(@sp.group_participations.find_by(akadem_group_id: @new_ag.id).leave_date).to be_nil }
+      And   { expect(@sp.group_participations.find_by(akadem_group_id: @old_ag.id).leave_date).not_to be_nil }
+    end
+  end
 end
