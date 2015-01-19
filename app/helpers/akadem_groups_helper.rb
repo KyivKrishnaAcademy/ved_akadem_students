e2f60fde4ba98ccc84045582be77bb210ee13f3e
Maksym Pugach <pugach.m@gmail.com>
2015-01-19 14:00:04 +0200

    [#30] extract akademy group autocomplete tags to helper

---
+++ app/helpers/akadem_groups_helper.rb
@@ -0,0 +1,17 @@
+module AkademGroupsHelper
+  def people_autocomplete(f, field)
+    content_tag :div, class: ['form-group', field] do
+      div_content = ''
+
+      div_content << f.label(:administrator)
+      div_content << f.autocomplete_field(field,
+                                          autocomplete_person_akadem_groups_path,
+                                          id_element: "#akadem_group_#{field}_id",
+                                          class: 'form-control',
+                                          value: f.object.send(field).present? ? f.object.send(field).complex_name : '')
+      div_content << f.hidden_field("#{field}_id")
+
+      div_content.html_safe
+    end
+  end
+end
