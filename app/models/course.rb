953fac8cd6a181822ceff5894687dcaaae4ee466
Maksym Pugach <pugach.m@gmail.com>
2015-02-28 16:06:00 +0200

    [#57] validate presence of #name and #description for Course

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,6 @@ class Course < ActiveRecord::Base
   has_many :class_schedules, dependent: :destroy
   has_many :teacher_specialities, dependent: :destroy
   has_many :teacher_profiles, through: :teacher_specialities
+
+  validates :name, :description, presence: true
 end
--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 describe Course do
+  describe 'validations' do
+    Then { is_expected.to validate_presence_of(:name) }
+    Then { is_expected.to validate_presence_of(:description) }
+  end
+
   describe 'associations' do
     Then { is_expected.to have_many(:teacher_specialities).dependent(:destroy) }
     Then { is_expected.to have_many(:teacher_profiles).through(:teacher_specialities) }
