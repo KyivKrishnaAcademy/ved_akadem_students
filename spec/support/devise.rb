1ce2fbed1cf10a3f7885edb5ea72ce28e1ffa933
Maksym Pugach <pugach.m@gmail.com>
2015-05-19 06:30:17 +0300

    extract Warden into own helper

--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -1,9 +1,4 @@
 RSpec.configure do |config|
   config.include Devise::TestHelpers, type: :controller
   config.include Devise::TestHelpers, type: :view
-  config.include Warden::Test::Helpers
-
-  config.after :each do
-    Warden.test_reset!
-  end
 end
