a61a022db4f2e13f47ee6816450a12288529dde7
Maksym Pugach <pugach.m@gmail.com>
2015-06-03 06:18:14 +0300

    [#71] ClassScheduleHelper#time_value

--- app/helpers/class_schedules_helper.rb
+++ app/helpers/class_schedules_helper.rb
@@ -3,4 +3,8 @@ module ClassSchedulesHelper
     @_class_schedule_title ||=
       "#{class_schedule.course.title}, #{l(class_schedule.start_time, format: :with_day)}, #{class_schedule.classroom.title}"
   end
+
+  def time_value(time)
+    time.present? ? l(time, format: :time_picker) : ''
+  end
 end
--- spec/helpers/class_schedules_helper_spec.rb
+++ spec/helpers/class_schedules_helper_spec.rb
@@ -8,4 +8,9 @@ describe ClassSchedulesHelper do
 
     Then { expect(class_schedule_title(class_schedule)).to eq('Bhakti School, Ср 03.06.15 12:30, B4') }
   end
+
+  describe '#time_value' do
+    Then { expect(time_value(nil)).to eq('') }
+    Then { expect(time_value(DateTime.parse('Ср 03.06.2015 12:30'))).to eq('03.06.2015 12:30') }
+  end
 end
