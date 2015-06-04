83ef1386b51d6b2916af772a3c3864601ba509dc
Maksym Pugach <pugach.m@gmail.com>
2015-06-04 06:30:25 +0300

    [#71] validate roominess of ClassSchedule#classroom

--- app/models/class_schedule.rb
+++ app/models/class_schedule.rb
@@ -9,4 +9,21 @@ class ClassSchedule < ActiveRecord::Base
   has_many :attendances, dependent: :destroy
 
   validates :course, :classroom, :start_time, :finish_time, presence: true
+
+  validate :enough_roominess
+
+  private
+
+  def enough_roominess
+    return if classroom.blank? || academic_groups.none?
+
+    required_roominess = academic_groups.map { |ag| ag.active_students.count }.inject(:+)
+
+    return if required_roominess <= classroom.roominess
+
+    errors.add(:classroom,
+               I18n.t('activerecord.errors.models.class_schedule.attributes.classroom.roominess',
+                      actual: classroom.roominess,
+                      required: required_roominess))
+  end
 end
--- spec/models/class_schedule_spec.rb
+++ spec/models/class_schedule_spec.rb
@@ -19,5 +19,38 @@ describe ClassSchedule do
       Then { is_expected.to validate_presence_of(:start_time) }
       Then { is_expected.to validate_presence_of(:finish_time) }
     end
+
+    describe 'custom' do
+      describe 'classroom roominess' do
+        Given(:group) { create :academic_group}
+        Given(:student) { create :person }
+        Given(:schedule) { build :class_schedule, academic_groups: [group], classroom: classroom }
+        Given(:classroom) { create :classroom }
+        Given(:error_message) { I18n.t('activerecord.errors.models.class_schedule.attributes.classroom.roominess',
+                                       actual: 0,
+                                       required: 1) }
+
+        Given { student.create_student_profile.move_to_group(group) }
+
+        describe 'invalid' do
+          Then { expect(schedule).not_to be_valid }
+          And  { expect(schedule.errors.messages[:classroom]).to eq([error_message]) }
+        end
+
+        describe 'valid' do
+          context 'no groups' do
+            Given(:schedule) { build :class_schedule, classroom: classroom }
+
+            Then { expect(schedule).to be_valid }
+          end
+
+          context 'good roominess' do
+            Given(:classroom) { create :classroom, roominess: 1 }
+
+            Then { expect(schedule).to be_valid }
+          end
+        end
+      end
+    end
   end
 end
