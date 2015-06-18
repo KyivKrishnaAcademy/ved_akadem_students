c74689f821a20dc66b7a135c654e3d172ec21d73
Maksym Pugach <pugach.m@gmail.com>
2015-06-18 23:11:11 +0300

    [#52] render ClassSchedules table on home page

--- spec/views/static_pages/home.html.haml_spec.rb
+++ spec/views/static_pages/home.html.haml_spec.rb
@@ -10,6 +10,7 @@ describe 'static_pages/home' do
   Given { assign(:programs, []) }
   Given { assign(:new_study_application, new_study_application) }
   Given { allow(view).to receive(:current_person).and_return(user) }
+  Given { allow(view).to receive(:policy).with(ClassSchedule).and_return(Pundit.policy(user, ClassSchedule)) }
 
   When  { render }
 
