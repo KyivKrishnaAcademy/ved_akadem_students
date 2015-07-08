2cd43c96389bf6608dbb66f78b08ba313cf22d13
Maksym Pugach <pugach.m@gmail.com>
2015-07-08 07:15:12 +0300

    [#52][#51] do not show empty schedules table on dashboard

--- spec/views/static_pages/home.html.haml_spec.rb
+++ spec/views/static_pages/home.html.haml_spec.rb
@@ -12,6 +12,7 @@ describe 'static_pages/home' do
   Given { allow(view).to receive(:current_person).and_return(user) }
   Given { allow(view).to receive(:policy).with(ClassSchedule).and_return(Pundit.policy(user, ClassSchedule)) }
 
+  When  { init_schedules_mv }
   When  { render }
 
   describe 'user is a student' do
@@ -45,6 +46,42 @@ describe 'static_pages/home' do
 
       Then  { expect(rendered).to have_link(I18n.t('links.withdraw')) }
       And   { expect(rendered).not_to have_selector("input[type='submit'][name='commit'][value='#{I18n.t('links.apply_to_program')}']") }
+    end
+  end
+
+  describe 'schedules' do
+    Given!(:schedule) { create :class_schedule }
+
+    describe 'ordinary user' do
+      Then { expect(rendered).not_to have_content(I18n.t('static_pages.home.schedules_title')) }
+    end
+
+    describe 'student' do
+      Given { user.create_student_profile.move_to_group(group) }
+
+      context 'with schedule' do
+        Given { schedule.academic_groups << group }
+
+        Then  { expect(rendered).to have_content(I18n.t('static_pages.home.schedules_title')) }
+      end
+
+      context 'without schedule' do
+        Then { expect(rendered).not_to have_content(I18n.t('static_pages.home.schedules_title')) }
+      end
+    end
+
+    describe 'teacher' do
+      Given!(:teacher_profile) { user.create_teacher_profile }
+
+      context 'with schedule' do
+        Given { schedule.update_column(:teacher_profile_id, teacher_profile.id ) }
+
+        Then  { expect(rendered).to have_content(I18n.t('static_pages.home.schedules_title')) }
+      end
+
+      context 'without schedule' do
+        Then { expect(rendered).not_to have_content(I18n.t('static_pages.home.schedules_title')) }
+      end
     end
   end
 end
