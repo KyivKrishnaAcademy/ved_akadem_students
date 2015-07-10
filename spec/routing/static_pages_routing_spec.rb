9947781a401ab65fc04574230d38e75b98d99acf
Maksym Pugach <pugach.m@gmail.com>
2015-07-10 06:53:38 +0300

    [#16] remove About page tests

--- spec/routing/static_pages_routing_spec.rb
+++ spec/routing/static_pages_routing_spec.rb
@@ -2,5 +2,4 @@ require 'rails_helper'
 
 describe 'routes for StaticPages' do
   it { expect(get: '/static_pages/home' ).to route_to('static_pages#home' ) }
-  it { expect(get: '/static_pages/about').to route_to('static_pages#about') }
 end
