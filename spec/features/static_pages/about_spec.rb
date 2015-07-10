aef52a2fb1becc7f3cfaa74c7dd7c29c55ddd1c0
Maksym Pugach <pugach.m@gmail.com>
2015-07-10 06:51:20 +0300

    [#16] remove About page tests

--- spec/features/static_pages/about_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-describe :about do
-  it 'should have the right title' do
-    visit '/static_pages/about'
-    expect(page).to have_title("#{I18n.t :application_title} | About")
-  end
-end
