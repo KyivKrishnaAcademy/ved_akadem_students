bb7a6dc22781f791e1685191552199787aa8a51a
Maksym Pugach <pugach.m@gmail.com>
2015-07-10 06:26:32 +0300

    [#108] PeopleHelper.not_adult_warning

---
+++ spec/helpers/people_helper_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe PeopleHelper do
+  describe 'not_adult_warning' do
+    Then { expect(not_adult_warning(19.years.ago.to_date)).to be_blank }
+
+    Then { expect(not_adult_warning(17.years.ago.to_date)).to have_content(I18n.t('people.show.not_adult')) }
+  end
+end
