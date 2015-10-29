c94c8ef3edaaf931a686987026de28bc35896300
Maksym Pugach <pugach.m@gmail.com>
2015-10-29 06:38:15 +0200

    increase session expire time

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -6,7 +6,7 @@ Rails.application.config.session_store :redis_session_store, {
   key: '_vedic_academy_students_session',
   serializer: :json,
   redis: {
-    expire_after: 120.minutes,
+    expire_after: 1.week,
     key_prefix: 'myapp:session:'
   }
 }
