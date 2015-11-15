f6040704cd78f5e8924fa1b9b9a6e988f5e91009
Maksym Pugach <pugach.m@gmail.com>
2015-11-15 15:55:44 +0200

    fix locale toggling

--- app/controllers/locales_controller.rb
+++ app/controllers/locales_controller.rb
@@ -1,8 +1,8 @@
 class LocalesController < ApplicationController
-  skip_before_action :authenticate_person!
+  skip_before_action :authenticate_person!, :set_locale
 
   def toggle
-    session[:locale] = session[:locale] == :ru ? :uk : :ru
+    session[:locale] = I18n.locale.to_sym == :ru ? :uk : :ru
 
     redirect_to :back
   end
