e6454311e92682feefc594558856956b6802ec8c
Maksym Pugach <pugach.m@gmail.com>
2015-12-25 07:50:20 +0200

    [116] allow interaction to accept resource param

--- app/interactions/ui/person_group_updating_interaction.rb
+++ app/interactions/ui/person_group_updating_interaction.rb
@@ -1,10 +1,9 @@
 module Ui
   class PersonGroupUpdatingInteraction < BaseInteraction
     def init
-      person          = params.require(:person)
       @academic_group = AcademicGroup.find(params.require(:group_id))
 
-      (person.student_profile || person.create_student_profile).move_to_group(@academic_group)
+      (resource.student_profile || resource.create_student_profile).move_to_group(@academic_group)
     end
 
     def as_json(opts = {})
