f77adac05aec7416ff077fca684dca548ebd6edf
Maksym Pugach <pugach.m@gmail.com>
2016-02-21 22:27:55 +0200

    [137] load react only when needed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,7 +5,6 @@
 // since it is exposing jQuery and jQuery-ujs
 
 //= require generated/vendor-bundle
-//= require generated/app-bundle
 
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
