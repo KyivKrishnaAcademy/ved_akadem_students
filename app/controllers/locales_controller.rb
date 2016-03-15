1f43e275ed3eeb60ba82f365f4e228b06a78c096
legacy <korsair93@gmail.com>
2016-03-15 21:46:00 +0200

    refs #138: переделал глобальные переменные в локальные

--- app/controllers/locales_controller.rb
+++ app/controllers/locales_controller.rb
@@ -2,13 +2,13 @@ class LocalesController < ApplicationController
   skip_before_action :authenticate_person!
 
   def toggle
-    $locale = I18n.locale == :ru ? :uk : :ru
+    locale = I18n.locale == :ru ? :uk : :ru
     if current_person
-      $person = Person.find(current_person.id)
-      $person.locale = $locale
-      $person.save
+      person = Person.find(current_person.id)
+      person.locale = locale
+      person.save
     else
-      session[:locale] = $locale
+      session[:locale] = locale
     end
 
     redirect_to :back
