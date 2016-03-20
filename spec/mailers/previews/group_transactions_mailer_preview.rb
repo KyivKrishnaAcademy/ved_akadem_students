088e2d32d630e49eaef7e58a7b137a5d5de1e869
Maksym Pugach <pugach.m@gmail.com>
2016-03-20 02:28:10 +0200

    [67] DRY

--- spec/mailers/previews/group_transactions_mailer_preview.rb
+++ spec/mailers/previews/group_transactions_mailer_preview.rb
@@ -1,10 +1,16 @@
 # Preview all emails at http://localhost:3000/rails/mailers/group_transactions_mailer
 class GroupTransactionsMailerPreview < ActionMailer::Preview
   def leave_the_group_uk
-    GroupTransactionsMailer.leave_the_group(AcademicGroup.first, Person.where(locale: :uk).first)
+    leave_the_group(:uk)
   end
 
   def leave_the_group_ru
-    GroupTransactionsMailer.leave_the_group(AcademicGroup.first, Person.where(locale: :ru).first)
+    leave_the_group(:ru)
+  end
+
+  private
+
+  def leave_the_group(locale)
+    GroupTransactionsMailer.leave_the_group(AcademicGroup.first, Person.where(locale: locale).first)
   end
 end
