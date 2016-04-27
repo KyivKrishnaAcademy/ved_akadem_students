1a047dc673b4eed375f1bc839d01dca8df44ec04
Maksym Pugach <pugach.m@gmail.com>
2016-04-27 09:02:24 +0300

    [155] reply to coordinator

--- app/mailers/group_transactions_mailer.rb
+++ app/mailers/group_transactions_mailer.rb
@@ -4,7 +4,11 @@ class GroupTransactionsMailer < ApplicationMailer
     @person = person
 
     I18n.with_locale(person.locale) do
-      mail(to: person.email, subject: t('mail.group.leave.subject', group_title: group.title))
+      mail(
+        to: person.email,
+        reply_to: group.administrator.email,
+        subject: t('mail.group.leave.subject', group_title: group.title)
+      )
     end
   end
 end
