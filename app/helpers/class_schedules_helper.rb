164eca895d4c3ba1604e457e0ec020bdb01977d7
Maksym Pugach <pugach.m@gmail.com>
2016-09-16 06:24:24 +0300

    [143] fix helpers by Rubocop

--- app/helpers/class_schedules_helper.rb
+++ app/helpers/class_schedules_helper.rb
@@ -1,14 +1,14 @@
 module ClassSchedulesHelper
-  def class_schedule_title(class_schedule)
+  def class_schedule_title(c_schedule)
     @_class_schedule_title ||=
-      "#{class_schedule.course.title}, #{l(class_schedule.start_time, format: :with_day)}, #{class_schedule.classroom.title}"
+      "#{c_schedule.course.title}, #{l(c_schedule.start_time, format: :with_day)}, #{c_schedule.classroom.title}"
   end
 
   def time_value(time)
     time.present? ? l(time, format: :time_picker) : ''
   end
 
-  def show_scheduled_time(class_schedule)
-    "#{I18n.l(class_schedule.start_time, format: :with_day)} - #{I18n.l(class_schedule.finish_time, format: :just_time)}"
+  def show_scheduled_time(c_schedule)
+    "#{I18n.l(c_schedule.start_time, format: :with_day)} - #{I18n.l(c_schedule.finish_time, format: :just_time)}"
   end
 end
