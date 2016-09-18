c09bb94b9af4b8d79c0a5a46f4a79a31517e34f9
Maksym Pugach <pugach.m@gmail.com>
2016-09-18 22:24:49 +0300

    [143] fix spec/factories by Rubocop

--- spec/factories/questionnaire.rb
+++ spec/factories/questionnaire.rb
@@ -1,7 +1,9 @@
 FactoryGirl.define do
   psycho_test     = YAML.load_file(Rails.root.join('spec/fixtures/questionnaires/psyho_test_2.yml'))
-  psycho_options  = { ru: psycho_test[:answers][:ru].to_a.map(&:reverse),
-                      uk: psycho_test[:answers][:uk].to_a.map(&:reverse) }
+  psycho_options  = {
+    ru: psycho_test[:answers][:ru].to_a.map(&:reverse),
+    uk: psycho_test[:answers][:uk].to_a.map(&:reverse)
+  }
 
   initial_questions = YAML.load_file(Rails.root.join('spec/fixtures/questionnaires/initial_questions.yml'))
 
@@ -13,26 +15,42 @@ FactoryGirl.define do
     trait :psycho_test do
       kind      { 'psycho_test' }
       rule      { { keys: psycho_test[:keys], indexes: psycho_test[:indexes] } }
-      questions { psycho_test[:questions].first(11).map do |q|
-                    build(:question, :single_select,
-                          position:  q[:position],
-                          data:      {  text:       { uk: q[:question_uk],
-                                                      ru: q[:question_ru] },
-                                        options:    psycho_options,
-                                        key:        q[:key],
-                                        key_anwers: q[:key_answers].map(&:to_s) })
-                  end }
+
+      questions do
+        psycho_test[:questions].first(11).map do |q|
+          build(
+            :question,
+            :single_select,
+            position:  q[:position],
+            data: {
+              text: {
+                uk: q[:question_uk],
+                ru: q[:question_ru]
+              },
+              options:    psycho_options,
+              key:        q[:key],
+              key_anwers: q[:key_answers].map(&:to_s)
+            }
+          )
+        end
+      end
     end
 
     trait :initial do
       kind         { 'initial_questions' }
       title_uk     { initial_questions[:title_uk] }
       title_ru     { initial_questions[:title_ru] }
-      questions    { initial_questions[:questions].map do |q|
-                       build :question, :freeform,
-                             position: q[:position],
-                             data: { text: { uk: q[:question_uk], ru: q[:question_ru] } }
-                     end }
+
+      questions    do
+        initial_questions[:questions].map do |q|
+          build(
+            :question,
+            :freeform,
+            position: q[:position],
+            data: { text: { uk: q[:question_uk], ru: q[:question_ru] } }
+          )
+        end
+      end
     end
   end
 end
--- spec/factories/telephone.rb
+++ spec/factories/telephone.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
   factory :telephone do
-    sequence(:phone, 1_000_000 ) { |n| "+380 50 #{n.to_s.insert(3, ' ')}" }
+    sequence(:phone, 1_000_000) { |n| "+380 50 #{n.to_s.insert(3, ' ')}" }
   end
 end
