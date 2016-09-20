d5d738dc77a7a3bfcec65bd87358423615ee9c4d
Maksym Pugach <pugach.m@gmail.com>
2016-09-20 07:22:47 +0300

    [143] fix spec/helpers by Rubocop

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe ApplicationHelper do
   describe '#full_title' do
     it 'should include the page title and base title' do
-      expect(full_title('foo')).to eq(("#{t(:application_title)} | foo"))
+      expect(full_title('foo')).to eq("#{t(:application_title)} | foo")
     end
 
     it 'should not include a bar for the home page' do
