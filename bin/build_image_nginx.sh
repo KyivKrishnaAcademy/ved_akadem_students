2502d4b2d29b95ed12f4e915f4ca4828101d12d2
Maksym Pugach <pugach.m@gmail.com>
2016-11-20 06:59:50 +0200

    [175] regular nginx

--- bin/build_image_nginx.sh
+++ bin/build_image_nginx.sh
@@ -4,4 +4,4 @@ set -e
 
 . ./bin/lib/build_by_tag_and_dir.sh
 
-build_by_tag_and_dir 'mpugach' 'environmental_nginx' $(date "+%Y%m%d%H%M")
+build_by_tag_and_dir 'mpugach' 'akadem_students_nginx' $(date "+%Y%m%d%H%M")
