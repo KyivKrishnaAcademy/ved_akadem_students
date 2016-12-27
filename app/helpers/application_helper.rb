4e5bedc313e0a01a2f483e1ed5e36979fa2a4aab
Maksym Pugach <pugach.m@gmail.com>
2016-12-27 07:29:17 +0200

    [#116] split ApplicationHelper into concerns

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,7 @@
 module ApplicationHelper
+  include AbilityShortcuts
+  include LinkTos
+
   SIDEBAR_MENU = {
     person: { plural_name: :people, icon: :'fa-users' },
     course: { plural_name: :courses, icon: :'fa-book' },
@@ -24,30 +27,6 @@ module ApplicationHelper
     end
   end
 
-  def show_people_menu?
-    current_person.can_act?(%w(person:index person:new))
-  end
-
-  def show_academic_groups_menu?
-    current_person.can_act?(%w(academic_group:index academic_group:new))
-  end
-
-  def show_certificate_templates_menu?
-    current_person.can_act?(%w(certificate_template:index certificate_template:new))
-  end
-
-  def show_courses_menu?
-    current_person.can_act?(%w(course:index course:new))
-  end
-
-  def show_class_schedules_menu?
-    current_person.can_act?(%w(class_schedule:index class_schedule:new))
-  end
-
-  def show_journal_link?
-    current_person.can_act?('paper_trail/version:show')
-  end
-
   def person_photo(person, version = :default, options = {})
     if person.photo.present?
       image_tag "/people/show_photo/#{version}/#{person.id}", options
@@ -70,50 +49,9 @@ module ApplicationHelper
     # rubocop:enable Style/UnneededInterpolation
   end
 
-  def link_to_show_person_or_name(person, short = false)
-    link_to_if policy(person).show?, complex_name(person, short), person_path(person) do
-      complex_name(person, short)
-    end
-  end
-
   def class_schedules_table_headers
     %w(course teacher subject groups classroom time actions).map do |key|
       I18n.t("class_schedules.table_headers.#{key}")
-    end
-  end
-
-  def link_to_new(condition, path)
-    link_to_action(condition, path, 'success', t('links.new'), 'file')
-  end
-
-  def link_to_edit(condition, path)
-    link_to_action(condition, path, 'primary', t('links.edit'), 'pencil')
-  end
-
-  def link_to_index(condition, path)
-    link_to_action(condition, path, 'primary', t('links.list'), 'list')
-  end
-
-  def link_to_destroy(condition, path)
-    link_to_action(
-      condition, path, 'danger', t('links.delete'), 'trash',
-      data: { confirm: 'Are you sure?' }, method: :delete
-    )
-  end
-
-  def link_to_action(condition, path, btn_color, tooltip, icon, params = {})
-    return unless condition
-
-    link_params = {
-      class: "btn btn-xs btn-#{btn_color}",
-      data: { toggle: :tooltip },
-      title: tooltip
-    }
-
-    link_params.deep_merge!(params)
-
-    link_to path, link_params do
-      tag :span, class: "glyphicon glyphicon-#{icon}", aria: { hidden: true }
     end
   end
 
