d317dd2fc6897bde0c2425fee46a4420f257e925
Maksym Pugach <pugach.m@gmail.com>
2018-09-22 07:13:51 +0300

    [#267] lower the age limit to 16

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,6 +1,6 @@
 module PeopleHelper
   def not_adult_warning(birthday)
-    content_tag(:span, t('people.show.not_adult'), class: :'text-danger') if birthday > 18.years.ago
+    content_tag(:span, t('people.show.not_adult'), class: :'text-danger') if birthday > 16.years.ago
   end
 
   def leave_reason(group_participation)
--- spec/helpers/people_helper_spec.rb
+++ spec/helpers/people_helper_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe PeopleHelper do
   describe 'not_adult_warning' do
-    Then { expect(not_adult_warning(19.years.ago.to_date)).to be_blank }
+    Then { expect(not_adult_warning(17.years.ago.to_date)).to be_blank }
 
-    Then { expect(not_adult_warning(17.years.ago.to_date)).to have_content(I18n.t('people.show.not_adult')) }
+    Then { expect(not_adult_warning(15.years.ago.to_date)).to have_content(I18n.t('people.show.not_adult')) }
   end
 end
