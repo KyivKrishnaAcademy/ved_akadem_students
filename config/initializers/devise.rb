621d4c59f03c242a50795c92a87e53662ef83197
Maksym Pugach <pugach.m@gmail.com>
2020-01-19 14:12:46 +0200

    read mailer port and username from env

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'robodas@veda-kiev.org.ua'
+  config.mailer_sender = Rails.application.secrets.mailer_user_name
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
