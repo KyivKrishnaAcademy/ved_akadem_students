3f072c39136f293ccba241ca026967b895242f20
Maksym Pugach <pugach.m@gmail.com>
2020-12-20 10:49:29 +0200

    import certificates

--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -3,5 +3,5 @@ class Certificate < ApplicationRecord
   has_one :certificate_template
   has_one :student_profile
 
-  validates :serial_id, :issued_date, presence: true
+  validates :serial_id, :issued_date, :academic_group_id, :certificate_template_id, :student_profile_id, presence: true
 end
