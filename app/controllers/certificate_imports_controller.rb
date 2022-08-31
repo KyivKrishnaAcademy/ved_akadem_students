390ffce7de98d31e00a2ba29a828e18e600e8e57
Maksym Pugach <pugach.m@gmail.com>
2022-08-31 06:29:54 +0000

    Import certificates from CSV file

---
+++ app/controllers/certificate_imports_controller.rb
@@ -0,0 +1,26 @@
+class CertificateImportsController < HtmlRespondableController
+  after_action :verify_authorized
+
+  def new
+    authorize CertificateImportsController
+  end
+
+  def create
+    authorize CertificateImportsController
+
+    file = permitted_params.to_h.fetch(:certificate_import, {}).fetch(:file, nil)
+
+    if file.present?
+      @result = ImportCertificatesService.call(file.read)
+    else
+      redirect_to new_certificate_import_path, alert: t('certificate_imports.new.file_is_not_present_alert')
+
+    end
+  end
+
+  private
+
+  def permitted_params
+    params.permit(certificate_import: [:file])
+  end
+end
