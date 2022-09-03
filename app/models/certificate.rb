75e745282f1dd675b5aa037137e4708b27b82698
Maksym Pugach <pugach.m@gmail.com>
2022-09-03 06:20:57 +0000

    counter cache for issued certificates on the certificate templates list view

--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -1,6 +1,6 @@
 class Certificate < ApplicationRecord
   belongs_to :academic_group
-  belongs_to :certificate_template
+  belongs_to :certificate_template, counter_cache: true
   belongs_to :student_profile
 
   validates :serial_id, :issued_date, :academic_group_id, :certificate_template_id, :student_profile_id, presence: true
