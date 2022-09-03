0865742a24157f64e1a0619ecf93f81c1c63220f
Maksym Pugach <pugach.m@gmail.com>
2022-09-03 15:25:33 +0000

    Academic group is optional for certificates

--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -3,7 +3,7 @@ class Certificate < ApplicationRecord
   belongs_to :certificate_template, counter_cache: true
   belongs_to :student_profile
 
-  validates :serial_id, :issued_date, :academic_group_id, :certificate_template_id, :student_profile_id, presence: true
+  validates :serial_id, :issued_date, :certificate_template, :student_profile, presence: true
 
   has_paper_trail
 end
