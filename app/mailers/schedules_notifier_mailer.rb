5ee298f8fbae90592582f49ff84d7dfc171d7e6f
Maksym Pugach <pugach.m@gmail.com>
2024-11-16 18:40:50 +0200

    Add administrator contacts to the schedules email

--- app/mailers/schedules_notifier_mailer.rb
+++ app/mailers/schedules_notifier_mailer.rb
@@ -6,12 +6,13 @@ class SchedulesNotifierMailer < ApplicationMailer
       ClassScheduleWithPeople
         .next_day
         .order(:start_time)
-        .includes(:teacher, :classroom, :course, :academic_groups)
+        .includes(:teacher, :classroom, :course, academic_groups: [administrator: :telephones])
 
     @person = Person.find(person_id)
     @teacher_schedules = schedules.teacher_schedule(person_id)
     @student_schedules = schedules.student_schedule(person_id)
     @unsubscribe = Unsubscribe.generate(@person, 'notify_schedules')
+    @administrators = CollectAdministratorsBySchedulesService.call(schedules)
 
     I18n.with_locale(@person.locale) do
       mail(to: @person.email)
