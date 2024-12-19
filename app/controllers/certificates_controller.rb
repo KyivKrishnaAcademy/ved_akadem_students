4cd7fa95191889dc4f43780349647cdbc899c19c
Pavel <Pavel.Pasechnik@gmail.com>
2024-12-19 21:51:59 +0200

    The user can delete certificates

--- app/controllers/certificates_controller.rb
+++ app/controllers/certificates_controller.rb
@@ -1,18 +1,26 @@
 class CertificatesController < HtmlRespondableController
+  before_action :authenticate_person!
   after_action :verify_authorized
 
   def index
     @certificate_template = CertificateTemplate.find(params[:certificate_template_id])
-
-    @certificates =
-      Certificate
-        .where(certificate_template_id: params[:certificate_template_id])
-        .includes(:academic_group, student_profile: :person)
-        .order('people.complex_name ASC')
-        .page(params[:page])
-
+    @certificates = Certificate
+                      .where(certificate_template_id: params[:certificate_template_id])
+                      .includes(:academic_group, student_profile: :person)
+                      .order('people.complex_name ASC')
+                      .page(params[:page])
     authorize @certificates
-
     respond_with(@certificates)
+  end
+
+  def destroy
+    @certificate = Certificate.find_by(id: params[:id], certificate_template_id: params[:certificate_template_id])
+    authorize @certificate
+    if @certificate
+      @certificate.destroy
+      redirect_to request.referer || certificate_templates_path, notice: t('.success')
+    else
+      redirect_to request.referer || certificate_templates_path, alert: t('.not_found')
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20241126112155) do
+ActiveRecord::Schema.define(version: 20241219135735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
