bca147205bb753154351cf5c58c8eb8cb84ef3e8
Maksym Pugach <pugach.m@gmail.com>
2024-12-31 13:44:11 +0200

    Embed PhantomJS setup into dev environment
    Fix docker-compose.yml

--- bin/setup_phantomjs.sh
+++ bin/setup_phantomjs.sh
@@ -2,10 +2,7 @@
 
 echo "installing PhantomJS"
 
-export PHANTOMJS_VERSION=2.1.1
-export PHANTOMJS_DIR=$PROJECT_HOME/phantomjs
-export PATH=$PHANTOMJS_DIR/phantomjs-$PHANTOMJS_VERSION-linux-x86_64/bin:$PATH
-export OPENSSL_CONF=asdasdas
+export OPENSSL_CONF=none
 
 if (! [ -x "$(command -v phantomjs)" ]) || [ $(phantomjs --version) != $PHANTOMJS_VERSION ]; then
   rm -rf $PHANTOMJS_DIR
